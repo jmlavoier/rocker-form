50fd5aeb6f4665ab11bda18ac1b4d5f0c3f623ee
João Lavoier <jmlavoier@hotmail.com>
2020-03-03 21:01:37 -0300

    refactor(app): add form to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+
+import useForm from '~/hooks/useForm';
+
+import Input from '../Input';
+import Select from '../Select';
+import Button from '../Button';
+import Box from '../Box';
 import {
   Wrapper,
   Header,
@@ -6,27 +13,65 @@ import {
   Footer,
 } from './App.style';
 
-import Field from '../Field';
-import Button from '../Button';
-import Box from '../Box';
+const App = () => {
+  const [fields, changeField] = useForm('form', {
+    ssn: {
+      validation: (value) => (!value ? 'should have at least one character' : ''),
+    },
+    email: {
+      validation: (value) => (!value ? 'should have at least one character' : ''),
+    },
+    phone: {
+      validation: (value) => (!value ? 'should have at least one character' : ''),
+    },
+    country: {
+      validation: (value) => (!value ? 'should have at least one character' : ''),
+    },
+  });
 
-const App = () => (
-  <Wrapper>
-    <Box width="400px">
-      <Header>
-        Rocker Form
-      </Header>
-      <Content>
-        <Field name="ssn" />
-        <Field name="email" />
-        <Field name="phone" />
-        <Field name="country" />
-      </Content>
-      <Footer>
-        <Button>Submit</Button>
-      </Footer>
-    </Box>
-  </Wrapper>
-);
+  const {
+    ssn, email, phone, country,
+  } = fields;
+
+  return (
+    <Wrapper>
+      <Box width="400px">
+        <Header>
+          Rocker Form
+        </Header>
+        <Content>
+          <Input
+            name="ssn"
+            value={ssn.value}
+            onChange={changeField('ssn')}
+            errorMessage={ssn.errorMessage}
+          />
+          <Input
+            name="email"
+            value={email.value}
+            onChange={changeField('email')}
+            errorMessage={email.errorMessage}
+          />
+          <Input
+            name="phone"
+            value={phone.value}
+            onChange={changeField('phone')}
+            errorMessage={phone.errorMessage}
+          />
+          <Select
+            name="country"
+            value={country.value}
+            onChange={changeField('country')}
+            errorMessage={country.errorMessage}
+            options={['xa', 'xi']}
+          />
+        </Content>
+        <Footer>
+          <Button>Submit</Button>
+        </Footer>
+      </Box>
+    </Wrapper>
+  );
+};
 
 export default App;
